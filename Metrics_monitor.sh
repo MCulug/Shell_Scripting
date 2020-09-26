d4da5f632f74c3c1aa48399cc16a3250bf42c775
MCulug <41790613+MCulug@users.noreply.github.com>
2020-09-26 20:46:13 +0000

    Create Metrics_monitor.sh

---
+++ Metrics_monitor.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+i=1
+
+header= top -bn 1 | grep "^[0-9 ]" | awk '{ printf("%-8s %-8s %-8s \n", $12, $9, $10); }' | head -1
+
+header > metrics.txt     # top 3 cpu intensive process
+
+while i le 3;                 # top tree resource intensive command
+do
+
+Data_CPU_Sorted= top -bn 1 | grep "^[0-9 ]" | awk '{ printf("%-8s %-8s %-8s \n", $12, $9, $10); }' | head -$i | tail -n 1
+
+Data_Mem_Sorted= top -bn 1 | grep "^[0-9 ]" | awk '{ printf("%-8s %-8s %-8s \n", $12, $9, $10); }' | head -$i | tail -n 1
+
+Data_CPU_Sorted >> metrics.txt 
+Data_Mem_Sorted >> metrics.txt 
+
+
+sleep 3                                                                  # sleep interval                
+Done
